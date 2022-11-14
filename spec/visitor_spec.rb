77f916b7674da768232e2942882d40f9383dd766
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-11-14 08:11:07 -0800

    Add preferences tests and code

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Visitor do
     expect(visitor1.height).to eq(54)
     expect(visitor1.spending_money).to eq(10)
   end
+
+  it 'has preferences, empty by default' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    expect(visitor1.preferences).to eq([])
+  end
+
+  it 'can add preferences' do 
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor1.add_preference(:gentle)
+    visitor1.add_preference(:water)
+
+    expect(visitor1.preferences).to eq([:gentle, :water])
+  end
 end
